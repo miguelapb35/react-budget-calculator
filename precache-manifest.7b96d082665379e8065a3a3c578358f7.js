d36ab52a78b14150460d4d910a34fe86be6bff4d
Miguel Angel <miguelapb35@gmail.com>
2020-04-24 19:49:07 -0500

    Updates

---
+++ precache-manifest.7b96d082665379e8065a3a3c578358f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f2defa85148cca49ef3644b06fbfbc",
+    "url": "/react-budget-calculator/index.html"
+  },
+  {
+    "revision": "9aba741ea47f9139a94b",
+    "url": "/react-budget-calculator/static/css/main.3912b18a.chunk.css"
+  },
+  {
+    "revision": "35763d4c672ecb6fa9ab",
+    "url": "/react-budget-calculator/static/js/2.b679e9e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-budget-calculator/static/js/2.b679e9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aba741ea47f9139a94b",
+    "url": "/react-budget-calculator/static/js/main.8409bcb3.chunk.js"
+  },
+  {
+    "revision": "36f7c61382853846d60f",
+    "url": "/react-budget-calculator/static/js/runtime-main.10631d90.js"
+  }
+]);
